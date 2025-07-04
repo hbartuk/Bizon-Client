34f271496dce4c8641b18e971c392e0a85f278fd
hbartuk <186102957+hbartuk@users.noreply.github.com>
2025-07-04 09:24:06 +0300

    Update SkinCache.kt

--- app/src/main/java/com/retrivedmods/wclient/game/data/skin/SkinCache.kt
+++ app/src/main/java/com/retrivedmods/wclient/game/data/skin/SkinCache.kt
@@ -1,15 +1,15 @@
 package com.retrivedmods.wclient.game.data.skin
 
-import org.cloudburstmc.protocol.bedrock.data.skin.Skin
+import org.cloudburstmc.protocol.bedrock.data.skin.SerializedSkin
 
 object SkinCache {
-    private val skinMap: MutableMap<String, Skin> = mutableMapOf()
+    private val skinMap: MutableMap<String, SerializedSkin> = mutableMapOf()
 
-    fun putSkin(nick: String, skin: Skin) {
+    fun putSkin(nick: String, skin: SerializedSkin) {
         skinMap[nick.lowercase()] = skin
     }
 
-    fun getSkin(nick: String): Skin? {
+    fun getSkin(nick: String): SerializedSkin? {
         return skinMap[nick.lowercase()]
     }
 }
