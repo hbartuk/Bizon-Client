c86c280805cea8cd526306728fa1385d3700f857
hbartuk <186102957+hbartuk@users.noreply.github.com>
2025-07-31 17:08:13 +0300

    Update ModuleManager.kt

--- app/src/main/java/com/retrivedmods/wclient/game/ModuleManager.kt
+++ app/src/main/java/com/retrivedmods/wclient/game/ModuleManager.kt
@@ -76,7 +76,7 @@ object ModuleManager {
             add(AdvanceDisablerModule())
             add(BlinkModule()) // Дублирование, возможно, следует удалить одну
             add(NightVisionModule())
-            add(PingSpoofElement(session!!))
+            add(PingSpoofModule(session!!))
             add(RegenerationModule())
             add(AutoDisconnectModule())
             add(SkinStealerModule())
